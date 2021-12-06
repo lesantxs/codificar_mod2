c13fb8c0408749ed1250ed2fbc155e2452a59287
lesantxs <l.sancarmo@gmail.com>
2021-12-06 00:25:18 -0300

    js de navegação

---
+++ encaminha.js
@@ -0,0 +1,15 @@
+function encaminharIndex (){
+    return window.location.href = "./index.html"
+}
+
+function encaminharCesar (){
+    return window.location.href = "./cesar.html"
+}
+
+function encaminharBase64 (){
+    return window.location.href = "./base64.html"
+}
+
+function encaminharGitHub (){
+    return window.location.href = "https://github.com/lesantxs"
+}
